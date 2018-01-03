3ccc6c9606f4eb79d4bded3e7de6b6f479eb7964
ines-silc <ines.silc@gmail.com>
2018-01-03 20:41:07 +0100

    Ponoven poskus zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -80,7 +80,7 @@ avtomobili_graf
 # Uvozimo zemljevid.
 gpclibPermit()
 zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
-                             "shp/SVN_adm_shp",
+                             "SVN_adm_shp/SVN_adm1.shp",
                              encoding = "UTF-8") %>% pretvori.zemljevid()
 #levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
   #{ gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
@@ -88,10 +88,12 @@ zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm
 #zemljevid <- pretvori.zemljevid(zemljevid)
 ####
 
-#gpclibPermit()
-#zemljevid <- readShapeSpatial("C:/Users/Ines Šilc/Downloads/SVN_adm_shp/SVN_adm1.shp")
-#class(zemljevid)
-#plot(zemljevid)
+zemljevid <- readShapeSpatial("C:/Users/Ines Šilc/Downloads/SVN_adm_shp/SVN_adm1", 
+                              proj4string=CRS(as.character(NA)), 
+                              verbose=FALSE, repair=FALSE, IDvar=NULL, force_ring=FALSE, 
+                              delete_null_obj=FALSE, retrieve_ABS_null=FALSE)
+class(zemljevid)
+plot(zemljevid)
 #regije1 <- c("Pomurska", "Podravska", "Koroška", "Savinjska", "Zasavska", "Jugovzhodna Slovenija",
            #"Osrednjeslovenska", "Gorenjska", "Primorsko-notranjska", "Goriška", "Obalno-kraška")
 #regije2 <- c("Pomurska", "Podravska", "Koroška", "Savinjska", "Zasavska", "Jugovzhodna Slovenija",
